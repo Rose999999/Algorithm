01fc49e20934026a8e9ac057067910a7d9f35cf9
Rose9 <787998842@qq.com>
2020-11-29 10:53:23 +0800

    时钟的展示，分成两种情况，两个时钟的时间在同一天以及不在同一天(注：时间2在时间1之后，因为时间2后观察)

---
+++ timeclock.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int time1,hh1,mm1,ss1;
+	int time2,hh2,mm2,ss2;
+	scanf("%d:%d:%d",&hh1,&mm1,&ss1);
+	scanf("%d:%d:%d",&hh2,&mm2,&ss2);
+	time1 = hh1*3600+mm1*60+ss1;
+	time2 = hh2*3600+mm2*60+ss2;
+	if(time2 >= time1) printf("%d",time2-time1);
+	if(time2 < time1) printf("%d",time2+86400-time1);
+ 	return 0;
+}
